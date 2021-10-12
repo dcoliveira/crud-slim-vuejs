70f8e933ff122015e7e368ded15a69584d363d39
denison.oliveira <denison.oliveira@acpgroup.com.br>
2021-10-12 17:34:40 -0300

    ajustes em scripts sql

--- api/Docs/sql/create_database.sql
+++ api/Docs/sql/create_database.sql
@@ -2,28 +2,27 @@ create database api_slim character set utf8 collate utf8_general_ci;
 
 use api_slim;
 
-create table persons(
+create table users(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name VARCHAR (100) NOT NULL,
-    genre VARCHAR (1) NOT NULL,
-    cpf VARCHAR(20),
-	created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+	name VARCHAR (100) NOT NULL,
+    email VARCHAR (100) NOT NULL,
+    pass VARCHAR (45) NOT NULL,
+	phone_mobile VARCHAR(15) NOT NULL,
+    active TINYINT(1) NOT NULL,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME,
     PRIMARY KEY(id)
 );
 
-
-create table users(
+create table tokens(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    email VARCHAR (100) NOT NULL,
-    pass VARCHAR (45) NOT NULL,
-    active TINYINT(1) NOT NULL,
-    person_id INT UNSIGNED NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    token varchar(1000) NOT NULL,
+    refresh_token varchar(1000) NOT NULL,
+    expired_at DATETIME NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY (person_id)
-        REFERENCES persons(id)
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
         ON DELETE CASCADE
 );
 
@@ -41,9 +40,9 @@ create table collection(
     author VARCHAR (100) NOT NULL,
 	type_id INT UNSIGNED NOT NULL,
 	active TINYINT(1) NOT NULL,
-    PRIMARY KEY(id),
 	created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+    updated_at DATETIME,
+    PRIMARY KEY(id),
         FOREIGN KEY (type_id)
         REFERENCES type_collection(id)
         ON DELETE CASCADE
@@ -52,15 +51,15 @@ create table collection(
 create table loans(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     collection_id INT UNSIGNED NOT NULL,
-    person_id INT UNSIGNED NOT NULL,
-	loan_date DATETIME NOT NULL,
-    devolution_date DATETIME NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+	created_at DATETIME NOT NULL,
+    devolution_date DATETIME,
     PRIMARY KEY(id),
     FOREIGN KEY (collection_id)
         REFERENCES collection(id)
         ON DELETE CASCADE,
-	FOREIGN KEY (person_id)
-        REFERENCES persons(id)
+	FOREIGN KEY (user_id)
+        REFERENCES users(id)
         ON DELETE CASCADE
 );
 
--- api/Docs/sql/insert_tables.sql
+++ api/Docs/sql/insert_tables.sql
@@ -1,11 +1,8 @@
-insert into persons (name, genre, cpf, created_at, updated_at)
-values ('Denison Oliveira', 'M', '83446263358','2021-10-10 18:00:23','2021-10-10 18:00:23'),
-	   ('Luciana Oliveira', 'F', '83446263999','2021-10-10 18:00:23','2021-10-10 18:00:23');
+   
        
-       
-insert into users (email, pass, active, person_id, created_at, updated_at)
-values ('denison-costa@hotmail.com','123456',1,1,'2021-10-10 18:00:23','2021-10-10 18:00:23'),
-	   ('lbvidal@gmail.com','123456',1,2,'2021-10-10 18:00:23','2021-10-10 18:00:23');
+insert into users (name, email, pass, phone_mobile, active, created_at, updated_at)
+values ('Denison Costa de Oliveira','denison-costa@hotmail.com','123456','71991219741',1,1,'2021-10-10 18:00:23','2021-10-10 18:00:23'),
+	   ('Luciana Oliveira','lbvidal@gmail.com','123456','71999999999',1,2,'2021-10-10 18:00:23','2021-10-10 18:00:23');
 
 
 insert into type_collection (name, created_at, updated_at)
